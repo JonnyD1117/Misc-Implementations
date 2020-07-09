701d44689a782d526d7cb61d91eb91b20a8df45d
jtdorsey22 <jonathandorsey1117@gmail.com>
2020-07-09 03:34:27 -0700

    Alot of new files

---
+++ Numerical_Methods/EulersMethods/MassSpringDamper.m
@@ -0,0 +1,24 @@
+%% Eulers Method for Mass Spring & Damper System 
+
+% Parameters
+m = 10; 
+c  =1.; 
+k = 20; 
+
+% Input Signal
+
+F_in = 0;
+
+
+% Initial Conditions
+x_0 = ; 
+x_dot_0 = ; 
+
+
+
+
+% ODE Model
+
+
+
+x_ddot =  (F - c*x_dot - k*x)*(1/m);
