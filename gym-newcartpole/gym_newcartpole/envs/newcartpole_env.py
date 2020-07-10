1e98ff952bc94fab9b44a8f8ddfec382cac0d82e
jtdorsey22 <jonathandorsey1117@gmail.com>
2020-07-09 20:18:20 -0700

    Deleted OLD dynamics equations

--- gym-newcartpole/gym_newcartpole/envs/newcartpole_env.py
+++ gym-newcartpole/gym_newcartpole/envs/newcartpole_env.py
@@ -100,12 +100,6 @@ class CartPoleNewEnv(gym.Env):
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
 
-        # For the interested reader:
-        # https://coneural.org/florian/papers/05_cart_pole.pdf
-        # temp = (force + self.polemass_length * theta_dot ** 2 * sintheta) / self.total_mass
-        # thetaacc = (self.gravity * sintheta - costheta * temp) / (self.length * (4.0 / 3.0 - self.masspole * costheta ** 2 / self.total_mass))
-        # xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-
         # MATLAB
         # x_num = (mp * g * sin(theta) * cos(theta) - mp * L * sin(theta) * (theta_dot) ^ 2 + F_x);
         # x_den = ((m + mp) - mp * (cos(theta)) ^ 2);
